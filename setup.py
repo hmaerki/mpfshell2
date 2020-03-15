b87745692f09c28584230d69a3852cde039cf40b
Hans Märki <buhtig.hans.maerki@ergoinfo.ch>
2020-03-15 13:09:04 +0100

    Renamed mpfshell -> mpfshell2. Updated version info.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 from mp import version
 
 setup(
-    name="mpfshell",
+    name="mpfshell2",
     version=version.FULL,
     description="A simple shell based file explorer for ESP8266 and WiPy "
     "Micropython devices.",
-    author="Stefan Wendler",
-    author_email="sw@kaltpost.de",
-    url="https://github.com/wendlers/mpfshell",
-    download_url="https://github.com/wendlers/mpfshell/archive/0.8.1.tar.gz",
+    author="Stefan Wendler, with extensions by Hans Maerki",
+    author_email="sw@kaltpost.de, hans@maerki.com",
+    url="https://github.com/hmaerki/mpfshell2",
+    download_url="https://github.com/hmaerki/mpfshell2/archive/0.9.1.tar.gz",
     install_requires=["pyserial", "colorama", "websocket_client"],
     packages=["mp"],
     keywords=["micropython", "shell", "file transfer", "development"],
